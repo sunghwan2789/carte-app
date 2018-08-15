16c226a59bb60518a7167385b9da073d87d6fefb
Bang Sunghwan <sunghwan2789@gmail.com>
2018-08-15 11:25:59 +0900

    Add ability to change date.

--- src/components/DateNavigator.tsx
+++ src/components/DateNavigator.tsx
@@ -2,6 +2,7 @@ import * as React from 'react';
 import { IconButton, Typography } from '@material-ui/core';
 import { ChevronLeft, ChevronRight } from '@material-ui/icons';
 import { createStyles, WithStyles, withStyles } from '@material-ui/core/styles';
+import * as dayjs from 'dayjs';
 
 const styles = createStyles({
   navigator: {
@@ -17,19 +18,46 @@ const styles = createStyles({
 interface IProps extends WithStyles<typeof styles> {}
 
 export default withStyles(styles)(class extends React.Component<IProps> {
+  state = {
+    viewingDate: dayjs(),
+  }
+
+  handlePrevious() {
+    const { viewingDate } = this.state;
+    this.setState({
+      ...this.state,
+      // TODO: UnitType도 state로 관리
+      // 일간/주간/월간 식단
+      viewingDate: viewingDate.add(-1, 'day'),
+    })
+  }
+
+  handleNext() {
+    const { viewingDate } = this.state;
+    this.setState({
+      ...this.state,
+      // TODO: UnitType도 state로 관리
+      // 일간/주간/월간 식단
+      viewingDate: viewingDate.add(1, 'day'),
+    })
+  }
+
   render() {
     const { classes } = this.props;
+    const { viewingDate } = this.state;
 
     return (
       <React.Fragment>
-        <IconButton className={classes.navigator}>
+        <IconButton className={classes.navigator}
+          onClick={() => this.handlePrevious()}>
           <ChevronLeft />
         </IconButton>
-        <IconButton className={classes.navigator}>
+        <IconButton className={classes.navigator}
+          onClick={() => this.handleNext()}>
           <ChevronRight />
         </IconButton>
         <Typography variant="title" className={classes.date}>
-          WWhat
+          {viewingDate.format('YYYY년 M월 D일')}
         </Typography>
       </React.Fragment>
     );
