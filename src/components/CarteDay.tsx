e52e7fd44c7b1700668c832588414ec04398c7fe
Bang Sunghwan <sunghwan2789@gmail.com>
2018-08-19 14:46:08 +0900

    Design CarteDay

--- src/components/CarteDay.tsx
+++ src/components/CarteDay.tsx
@@ -1,11 +1,15 @@
 import * as React from 'react';
-import { WithStyles, createStyles, withStyles, Typography } from '@material-ui/core';
+import { WithStyles, createStyles, withStyles, Typography, AppBar, Tabs, Tab, Card, CardHeader, List, ListItem, ListItemText, Divider } from '@material-ui/core';
 import Carte from '../models/Carte';
 import CarteFood from './CarteFood';
+import Meal from '../models/Meal';
+import { observable, action } from 'mobx';
+import { observer } from 'mobx-react';
+import * as dayjs from 'dayjs';
 
 const styles = createStyles({
   root: {
-    color: 'inherit'
+    overflow: 'hidden',
   },
 });
 
@@ -14,7 +18,24 @@ interface IProps {
   isLoading: boolean
 }
 
+@observer
 class CarteDay extends React.Component<IProps & WithStyles<typeof styles>> {
+
+  @observable
+  currentMealName: string =
+    dayjs().hour() < 9 ? '조식'
+    : dayjs().hour() < 14 ? '중식'
+    : '석식';
+
+  @action
+  handleMealChange = (name: string) => {
+    this.currentMealName = name;
+  }
+
+  get currentMeal() {
+    return this.props.carte.meals.find(meal => meal.name === this.currentMealName);
+  }
+
   render() {
     const { classes } = this.props;
     if (!this.props.carte || this.props.isLoading) {
@@ -22,12 +43,26 @@ class CarteDay extends React.Component<IProps & WithStyles<typeof styles>> {
     }
     return (
       <div className={classes.root}>
-        {this.props.carte.meals.map(meal => (
-          <React.Fragment key={meal.name}>
-            <Typography variant="subheading">{meal.name}</Typography>
-            {meal.foods.map(food => <CarteFood key={food} food={food} />)}
-          </React.Fragment>
-        ))}
+        <AppBar position="static">
+          <Tabs
+            value={this.currentMealName}
+            onChange={(e, v) => this.handleMealChange(v)}
+          >
+            {this.props.carte.meals.map(meal => (
+              <Tab key={meal.name} value={meal.name} label={meal.name} />
+            ))}
+          </Tabs>
+        </AppBar>
+        <List>
+          {this.currentMeal && this.currentMeal.foods.map(food => (
+            <React.Fragment>
+              <ListItem key={food} style={{backgroundColor:'white'}}>
+                <ListItemText primary={food} />
+              </ListItem>
+              <Divider />
+            </React.Fragment>
+          ))}
+        </List>
       </div>
     );
   }
