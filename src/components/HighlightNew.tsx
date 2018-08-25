44c8f25b17eac7e4c547f01d536fcb90660a465b
Bang Sunghwan <sunghwan2789@gmail.com>
2018-08-25 17:23:39 +0900

    Add color change feature

--- src/components/HighlightNew.tsx
+++ src/components/HighlightNew.tsx
@@ -11,9 +11,48 @@ import Highlight from '../models/Highlight';
 import highlightStore from '../stores/highlightStore';
 import TextField from '@material-ui/core/TextField';
 import Switch from '@material-ui/core/Switch';
-import { List, ListItem, ListSubheader, Divider, ListItemSecondaryAction, ListItemText } from '@material-ui/core';
+import { List, ListItem, ListSubheader, Divider, ListItemSecondaryAction, ListItemText, Dialog, DialogTitle, DialogContent, DialogActions, Button } from '@material-ui/core';
 import { observer } from 'mobx-react';
 import { observable, action } from 'mobx';
+import { CompactPicker, ColorResult } from 'react-color';
+import { DialogProps } from '@material-ui/core/Dialog';
+
+interface IColorPickDialogProps {
+  title: string
+  name: string
+  handleClose: (value?: string) => void
+}
+
+class ColorPickDialog extends React.Component<IColorPickDialogProps & DialogProps> {
+  value: string
+
+  handleChange = (color: ColorResult) => {
+    this.value = color.hex;
+  }
+
+  handleCancel = () => {
+    this.props.handleClose();
+  }
+
+  handleOk = () => {
+    this.props.handleClose(this.value);
+  }
+
+  render() {
+    const { title, name, ...other } = this.props;
+
+    return (
+      <Dialog {...other}>
+        <DialogTitle>{title}</DialogTitle>
+        <DialogContent><CompactPicker onChangeComplete={this.handleChange} /></DialogContent>
+        <DialogActions>
+          <Button onClick={this.handleCancel} color="primary">취소</Button>
+          <Button onClick={this.handleOk} color="primary">확인</Button>
+        </DialogActions>
+      </Dialog>
+    );
+  }
+}
 
 const styles = (theme: Theme) => createStyles({
   list: {
@@ -59,6 +98,51 @@ class HighlightNew extends React.Component<RouteComponentProps<any> & WithStyles
     this.fontWeight = !this.fontWeight ? 'bold' : undefined;
   }
 
+  @observable
+  colorPickerTitle: string
+  @observable
+  colorPickerName: string
+  @observable
+  colorPickerPicking: boolean = false
+
+  @action
+  pickColor(title: string, name: string) {
+    this.colorPickerPicking = true;
+    this.colorPickerTitle = title;
+    this.colorPickerName = name;
+  }
+
+  @action
+  handleColorChange = (color?: string) => {
+    this.colorPickerPicking = false;
+    if (typeof color === 'undefined') {
+      return;
+    }
+
+    switch (this.colorPickerName) {
+      case 'backgroundColor':
+        this.backgroundColor = color;
+        break;
+      case 'color':
+        this.color = color;
+        break;
+    }
+  }
+
+  @action
+  toggleBackgroundColor = () => {
+    if (this.backgroundColor) {
+      this.backgroundColor = undefined;
+    }
+  }
+
+  @action
+  toggleColor = () => {
+    if (this.color) {
+      this.color = undefined;
+    }
+  }
+
   render() {
     return (
       <form onSubmit={this.handleSave}>
@@ -83,18 +167,22 @@ class HighlightNew extends React.Component<RouteComponentProps<any> & WithStyles
         </List>
         <List subheader={<ListSubheader disableSticky>스타일</ListSubheader>}>
           <ListItem>
-            <ListItemText primary="미리보기" primaryTypographyProps={{ style: this.style }} />
+            <ListItemText primary="미리보기" primaryTypographyProps={{ style: { ...this.style, display: 'inline' } }} />
           </ListItem>
-          <ListItem button>
+          <ListItem button onClick={() => this.pickColor('배경색 선택', 'backgroundColor')}>
             <ListItemText primary="배경색" secondary={this.backgroundColor} />
             <ListItemSecondaryAction>
-              <Switch checked={!!this.backgroundColor} />
+              <Switch checked={!!this.backgroundColor}
+                onClick={() => !this.backgroundColor ? this.pickColor('배경색 선택', 'backgroundColor') : this.toggleBackgroundColor()}
+              />
             </ListItemSecondaryAction>
           </ListItem>
-          <ListItem button>
+          <ListItem button onClick={() => this.pickColor('글자색 선택', 'color')}>
             <ListItemText primary="글자색" secondary={this.color} />
             <ListItemSecondaryAction>
-              <Switch checked={!!this.color} />
+              <Switch checked={!!this.color}
+                onClick={() => !this.color ? this.pickColor('글자색 선택', 'color') : this.toggleColor()}
+              />
             </ListItemSecondaryAction>
           </ListItem>
           <ListItem button onClick={this.toggleBold}>
@@ -105,7 +193,12 @@ class HighlightNew extends React.Component<RouteComponentProps<any> & WithStyles
             </ListItemSecondaryAction>
           </ListItem>
         </List>
-
+        <ColorPickDialog
+          open={this.colorPickerPicking}
+          title={this.colorPickerTitle}
+          name={this.colorPickerName}
+          handleClose={this.handleColorChange}
+        />
       </form>
     );
   }
