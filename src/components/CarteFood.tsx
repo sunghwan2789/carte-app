b1368b1915285eb2485dc218703ba333076517fe
Sunghwan Bang <sunghwan2789@gmail.com>
2020-04-30 12:01:45 +0900

    convert CarteFood

---
+++ src/components/CarteFood.tsx
@@ -0,0 +1,39 @@
+import React, { useMemo } from 'react';
+import { useHighlights } from '../contexts/HighlightsContext';
+
+type CarteFoodProps = {
+  food: string;
+};
+
+export default function CarteFood({ food }: CarteFoodProps) {
+  const [highlights] = useHighlights();
+
+  const { titleChunks, styleChunks } = useMemo(() => {
+    let titleChunks = Array.from(Array(food.length), () => '');
+    let styleChunks = Array.from(Array(food.length), () => ({}));
+
+    [...highlights!].reverse().forEach((highlight) => {
+      highlight.words.forEach((word) => {
+        let i = -1;
+        while ((i = food.indexOf(word, i + 1)) !== -1) {
+          for (let j = 0; j < word.length; j++) {
+            titleChunks[i + j] += `${highlight.name} `;
+            Object.assign(styleChunks[i + j], highlight.style);
+          }
+        }
+      });
+    });
+
+    return { titleChunks, styleChunks };
+  }, [highlights, food]);
+
+  return (
+    <>
+      {food.split('').map((char, i) => (
+        <span key={i} title={titleChunks[i]} style={styleChunks[i]}>
+          {char}
+        </span>
+      ))}
+    </>
+  );
+}
