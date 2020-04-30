c2fe193a5e7101f1fb7ae2178fc9fe62fb5a9a2e
Sunghwan Bang <sunghwan2789@gmail.com>
2020-04-30 10:00:34 +0900

    convert NavigateButtons

---
+++ src/components/NavigateButtons.tsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import { createStyles, makeStyles } from '@material-ui/core/styles';
+import IconButton from '@material-ui/core/IconButton';
+import ChevronLeft from '@material-ui/icons/ChevronLeft';
+import ChevronRight from '@material-ui/icons/ChevronRight';
+
+type NavigateButtonsProps = {
+  handleBackward?: () => void;
+  handleForward?: () => void;
+};
+
+export default function NavigateButtons({
+  handleBackward,
+  handleForward,
+}: NavigateButtonsProps) {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.root}>
+      <IconButton className={classes.btn} onClick={handleBackward}>
+        <ChevronLeft />
+      </IconButton>
+      <IconButton className={classes.btn} onClick={handleForward}>
+        <ChevronRight />
+      </IconButton>
+    </div>
+  );
+}
+
+const useStyles = makeStyles((theme) =>
+  createStyles({
+    root: {
+      marginLeft: 8,
+      marginRight: 8,
+      display: 'flex',
+    },
+    btn: {
+      width: 32,
+      height: 32,
+      padding: '0 12px',
+      color: 'inherit',
+    },
+  })
+);
