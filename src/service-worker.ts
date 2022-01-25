7930e5c66b5fd3918337dc1e5838306bacc37477
Sunghwan Bang <sunghwan2789@gmail.com>
2022-01-26 01:17:20 +0900

    serve cached static files

--- src/service-worker.ts
+++ src/service-worker.ts
@@ -13,3 +13,12 @@ async function activate() {
   await Promise.all(keys.map((key) => key !== version && caches.delete(key)))
 }
 self.addEventListener('activate', (e) => e.waitUntil(activate()))
+
+self.addEventListener('fetch', (e) =>
+  e.respondWith(
+    caches
+      .match(e.request)
+      .then((cachedResponse) => cachedResponse ?? fetch(e.request))
+      .catch(() => caches.match('index.html') as Promise<Response>)
+  )
+)
