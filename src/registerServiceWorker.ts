60c45b1af7a1e90b5a28b7eaab8b90436696e47b
Sunghwan Bang <sunghwan2789@gmail.com>
2022-01-25 21:19:04 +0900

    enable service worker global scope linting

--- src/registerServiceWorker.ts
+++ src/registerServiceWorker.ts
@@ -1,10 +1,10 @@
 export default function registerServiceWorker() {
-  if (process.env.NODE_ENV === 'production' && 'serviceWorker' in navigator) {
-    window.addEventListener('load', () => {
-      navigator.serviceWorker.register(
-        new URL('service-worker.ts', import.meta.url),
-        { type: 'module' }
-      )
-    })
-  }
+  // if (process.env.NODE_ENV === 'production' && 'serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register(
+      new URL('service-worker.ts', import.meta.url),
+      { type: 'module' }
+    )
+  })
+  // }
 }
